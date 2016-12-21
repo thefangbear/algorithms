e5acfb094a510462a28350a2d10be146680d61e3
Alex Fang <frjalex@temple.edu>
2016-12-20 19:33:03 -0500

    Create kandr_qsort_impl.c

---
+++ sort/kandr_qsort_impl.c
@@ -0,0 +1,15 @@
+void qsort(void *v[], int left, int right, int (*comp)(void *, void *)) {
+    int i, last;
+    void swap(void *v[], int, int);
+
+    if (left >= right)
+        return;
+    swap(v, left, (left + right)/2);
+    last = left;
+    for (i = left+1; i <= right; i++)
+        if ((*comp)(v[i], v[left]) < 0) /* Here's the function call */
+            swap(v, ++last, i);
+    swap(v, left, last);
+    qsort(v, left, last-1, comp);
+    qsort(v, last+1, right, comp);
+}
