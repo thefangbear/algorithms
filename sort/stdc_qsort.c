da2f4bce63ddcfb5e5952705ff2db965a6f8f44b
Alex Fang <frjalex@temple.edu>
2016-12-22 13:01:40 -0500

    Update stdc_qsort.c

--- sort/stdc_qsort.c
+++ sort/stdc_qsort.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-int comp (const void * elem1, const void * elem2) 
-{
+
+#define MAX_SIZE 10240000
+
+int comp (const void * elem1, const void * elem2)  {
     int f = *((int*)elem1);
     int s = *((int*)elem2);
     if (f > s) return  1;
     if (f < s) return -1;
     return 0;
 }
-int main(int argc, char* argv[]) 
-{
-    int x[] = {4,5,2,3,1,0,9,8,6,7};
 
-    qsort (x, sizeof(x)/sizeof(*x), sizeof(*x), comp);
 
-    for (int i = 0 ; i < 10 ; i++)
+int main(int argc, char* argv[]) {
+    int *x = (int *)malloc(sizeof(int) * MAX_SIZE );
+    int i, j;
+    
+    for(i = 0; i < MAX_SIZE; i++) {
+        printf("%d\n", x[i]);
+    }
+    
+    qsort (x,           /* array/memory space to be sorted */
+           MAX_SIZE,    /* total number of elements        */ 
+           sizeof(int), /* size of one element in array    */
+           comp         /* pointer to comparator function  */
+          );
+
+    
+    for (i ; i > 0 ; i--) {
         printf ("%d ", x[i]);
-
+    }
+    
     return 0;
 }
