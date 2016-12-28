d5e16cddf9ad536b4b7d83766222103a7a34c9e2
Alex Fang <frjalex@temple.edu>
2016-12-27 23:26:41 -0500

    Revised linked list

--- ds/linked_list.c
+++ ds/linked_list.c
@@ -1,49 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
+typedef struct {
     int val;
     struct node * next;
-} node_t;
+} ListNode_t;
 
-void push(node_t * head, int val) {
-    node_t * current = head;
-    while (current->next != NULL) {
-        current = current->next;
+ListNode_t * init() {
+    ListNode_t *node = (ListNode_t *)malloc(sizeof(ListNode_t));
+    node->val = 0;
+    node->next = NULL;
+    return node;
+}
+
+void add_space(ListNode_t *list, unsigned int amount) {
+    ListNode_t *lp;
+    for(lp = list; lp != NULL; lp = lp->next);
+    
+    unsigned int i;
+    for(i = 0; i < amount, i++) {
+        lp->next = (ListNode_t *)malloc(sizeof(ListNode_t));
+        lp = lp->next;
+        lp->val = 0;
+        lp->next = NULL;
     }
-
-    /* now we can add a new variable */
-    current->next = malloc(sizeof(node_t));
-    current->next->val = val;
-    current->next->next = NULL;
 }
 
-void print_list(node_t * head) {
-    node_t * current = head;
+ListNode_t * find_predecessor(ListNode_t *element, ListNode_t *list) {
+    ListNode_t *lp;
+    for(lp = list; lp != NULL; lp = lp->next) {
+        if(lp->next == element)
+            return lp;
+    }
+    return NULL;
+}
 
-    while (current != NULL) {
-        printf("%d\n", current->val);
-        current = current->next;
+ListNode_t * find_successor(ListNode_t *element, ListNode_t *list) {
+    ListNode_t *lp;
+    for(lp = list; lp != NULL; lp = lp->next) {
+        if(element == lp->next)
+            return lp->next;
+    }
+    return NULL;
+}
+
+void insert_after(ListNode_t *predecessor, ListNode_t *element, ListNode_t *list) {
+    ListNode_t *lp, *lpp = NULL;
+    for(lp = list; lp != NULL; lp = lp->next) {
+        if(lp == predecessor && lp->next != NULL) {
+            element->next = lp->next;
+            lp->next = element;
+        }
     }
 }
 
-node_t init() {
+void insert_before(ListNode_t *successor, ListNode_t *element, ListNode_t *list) {
+    if(successor == list)
+        element->next = successor;
+    else {
+        ListNode_t *lp;
+        for(lp = list; lp != NULL; lp = lp->next) {
+            if(lp->next == successor) {
+                element->next = successor;
+                lp->next = element;
+                return;
+            }
+        }
+        // shouldn't reach here, really.
+    }
+}
+// TODO
+void reverse(){
 
-  node_t * head = NULL;
-  head = malloc(sizeof(node_t));
-  if (head == NULL) {
-     return 1;
-  }
-
-  head->val = 1;
-  head->next = NULL;
-  return head;
 }
 
-void add(node_t *head) {
-  head = malloc(sizeof(node_t));
-  head->val = 1;
-  head->next = malloc(sizeof(node_t));
-  head->next->val = 2;
-  head->next->next = NULL;
+void for_loop_traverse(ListNode_t *list, void callbackFunc(), void *userData) {
+    ListNode_t *lp;
+    for(lp = list; lp != NULL; lp = lp->next) {
+        callbackFunc(lp, userData);
+    }
+}
+
+void while_loop_traverse(ListNode_t *list, void callbackFunc(), void *userData) {
+    while(list != NULL) {
+        callbackFunc(list, userData);
+        list = list->next;
+    }
 }
